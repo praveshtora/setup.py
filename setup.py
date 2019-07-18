82f08adb15164887883a2ac4427835316445688b
praveshtora <pravesh.tora@gmail.com>
2019-07-18 22:03:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,131 +1,88 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
-
-import io
-import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup, Command
-
-# Package meta-data.
-NAME = 'mypackage'
-DESCRIPTION = 'My short description for my project.'
-URL = 'https://github.com/me/myproject'
-EMAIL = 'me@example.com'
-AUTHOR = 'Awesome Soul'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=['urllib3==1.24.2', 'pymongo==3.7.2', 'rabbitpy==1.0', 'pycryptodome==3.7.3']
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
+    author='Simon Davy',
+    author_email='simon.davy@canonical.com',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: System :: Logging',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    description='A common WSGI stack',
+    entry_points=dict(
+        console_scripts=[
+            'talisker=talisker:run_gunicorn',
+            'talisker.run=talisker:run',
+            'talisker.gunicorn=talisker:run_gunicorn',
+            'talisker.gunicorn.eventlet=talisker:run_gunicorn_eventlet',
+            'talisker.gunicorn.gevent=talisker:run_gunicorn_gevent',
+            'talisker.celery=talisker:run_celery',
+        ],
+    ),
+    extras_require=dict(
+        celery=[
+            'celery>=3.1.13.0,<5.0',
+        ],
+        dev=[
+            'logging_tree>=1.7',
+            'pygments>=2.2',
+            'psutil>=5.0',
+            'objgraph>=3.0',
+        ],
+        django=[
+            'django>=1.10,<2.0',
+        ],
+        flask=[
+            'flask>=0.11,<2.0',
+            'blinker>=1.4,<2.0',
+        ],
+        pg=[
+            'sqlparse',
+            'psycopg2',
+        ],
+        prometheus=[
+            'prometheus-client>=0.2.0,<0.5.0' + ',!=0.4.0,!=0.4.1',
+        ],
+    ),
+    include_package_data=True,
+    install_requires=[
+        'gunicorn>=19.7.0,<20.0',
+        'Werkzeug>=0.11.5,<0.15', 'statsd>=3.2.1,<4.0',
+        'requests>=2.10.0,<3.0', # renovate: ignore
+        'raven>=5.27.1,<7.0', # pyup: nothing
+        'future>=0.15.2,<0.17',
+        'ipaddress>=1.0.16,<2.0;python_version<"3.3"',
+    ],
+    keywords=[
+        'talisker',
+    ],
+    name='talisker',
+    package_data=dict(
+        talisker=[
+            'logstash/*',
+        ],
+    ),
+    package_dir=dict(
+        talisker='talisker',
+    ),
+    packages=[
+        'talisker',
+    ],
+    test_suite='tests',
+    url='https://github.com/canonical-ols/talisker',
+    version='0.9.16',
+    zip_safe=False,
 )
